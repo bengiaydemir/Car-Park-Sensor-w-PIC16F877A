ea4043faaec0a2a7e0591f2aec342cc8b32706d2
Bengisu Aydemir <121872241+bengiaydemir@users.noreply.github.com>
2024-06-12 16:52:46 +0300

    Update LED.c

--- LED_BLINKING.X/LED.c
+++ LED_BLINKING.X/LED.c
@@ -1,17 +1,15 @@
-// PIC16F877A LED blink example
-// http://simple-circuit.com/
- 
+
 #include <xc.h>
-#define _XTAL_FREQ 4000000 //define crystal frequency to 20MHz
+#define _XTAL_FREQ 4000000 //define crystal frequency to 4MHz
 
 void main(void) 
 {
-TRISB0=0; //set RC0 pin as a digital output pin
+TRISB0=0; //set RB0 pin as a digital output pin
 while(1)
 {
-   RB0 = 1;                  // set RC0 pin to logic High &  turn on 
+   RB0 = 1;                  // set RB0 pin to logic High &  turn on 
    __delay_ms(1000);  //add delay of 1 second 
-   RB0 = 0;                  // set RC0 pin to logic low & turn off
+   RB0 = 0;                  // set RB0 pin to logic low & turn off
   __delay_ms(1000);  //add delay of 1 second
 
 }
